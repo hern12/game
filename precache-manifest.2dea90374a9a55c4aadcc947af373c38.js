1a4037e977c48dbc8e59e77642c9416322a9180c
chilbi <13232158717@163.com>
2019-06-09 03:35:55 +0800

    Updates

---
+++ precache-manifest.2dea90374a9a55c4aadcc947af373c38.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205d55446858ad6fdd93a7bdcead8456",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "1cb3f9f6c0bbe4ef56f2",
+    "url": "/game/static/css/main.e94d129b.chunk.css"
+  },
+  {
+    "revision": "9d428cb26ab0b152d073",
+    "url": "/game/static/js/2.f23a9673.chunk.js"
+  },
+  {
+    "revision": "1cb3f9f6c0bbe4ef56f2",
+    "url": "/game/static/js/main.a2673276.chunk.js"
+  },
+  {
+    "revision": "bab8f84a0f4d6b7ca58b",
+    "url": "/game/static/js/runtime~main.2d0fbc73.js"
+  },
+  {
+    "revision": "204d74299df7396e4f51a4ee518b249d",
+    "url": "/game/static/media/iconfont.204d7429.svg"
+  },
+  {
+    "revision": "646d2bf2e5986917dcbf9bf6fd854aea",
+    "url": "/game/static/media/iconfont.646d2bf2.woff"
+  },
+  {
+    "revision": "686acc69166d2a941054caac8019280d",
+    "url": "/game/static/media/iconfont.686acc69.ttf"
+  },
+  {
+    "revision": "bdadb08222451ad206dc2750e862e8cb",
+    "url": "/game/static/media/iconfont.bdadb082.eot"
+  },
+  {
+    "revision": "c2b4940100258997181fc62ee5829046",
+    "url": "/game/static/media/iconfont.c2b49401.woff2"
+  },
+  {
+    "revision": "7d5eff42a3e4c04c0cb2addc72a79d88",
+    "url": "/game/static/media/move.7d5eff42.mp3"
+  },
+  {
+    "revision": "4af4d408da57cd7e27d7293569b8a2be",
+    "url": "/game/static/media/popup.4af4d408.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,39 @@
-"use strict";var precacheConfig=[["/game/index.html","0562839f583a0efd5ba98febffedc3ea"],["/game/static/css/main.4b1528b3.css","446408404ca71f55b5a2a3f7288701fc"],["/game/static/js/main.ce2ba450.js","5bee3df47f33f66fc13ebc6bb8fdf1d6"],["/game/static/media/iconfont.204d7429.svg","204d74299df7396e4f51a4ee518b249d"],["/game/static/media/iconfont.646d2bf2.woff","646d2bf2e5986917dcbf9bf6fd854aea"],["/game/static/media/iconfont.686acc69.ttf","686acc69166d2a941054caac8019280d"],["/game/static/media/iconfont.bdadb082.eot","bdadb08222451ad206dc2750e862e8cb"],["/game/static/media/iconfont.c2b49401.woff2","c2b4940100258997181fc62ee5829046"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,n,r){var a=new URL(e);return r&&a.pathname.match(r)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,n){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return n.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],r=new URL(t,self.location),a=createCacheKey(r,hashParamName,n,/\.\w{8}\./);return[r.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(r){return setOfCachedUrls(r).then(function(n){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!n.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return r.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var n=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!n.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,n=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),r="index.html";(e=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,r),e=urlsToCacheKeys.has(n));var a="/game/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(n=new URL(a,self.location).toString(),e=urlsToCacheKeys.has(n)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/game/precache-manifest.2dea90374a9a55c4aadcc947af373c38.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
